3e8d662edc35fe06e5ed0bd223530e7eab006f7b
NikolayL <lisitscky.k@yandex.ru>
2023-12-02 18:06:54 +0300

    class_VK.py changes

--- Code/class_vk.py
+++ Code/class_vk.py
@@ -15,31 +15,6 @@ class VK:
       self.params = {'access_token': self.token,
                     'v': self.version}
 
-    def users_info(self):
-        url = 'https://api.vk.com/method/users.get'
-        self.params.update({'user_id' : self.id})
-        response = requests.get(url, params=self.params)
-        return response.json()
-    
-
-    def get_status(self):
-        self.params.update({'user_id' : self.id})
-        response = requests.get(f'{self.API_BASE_URL}/status.get', params=self.params)
-        return response.json()['response']['text']
-    
-
-    def set_status(self, new_status):
-        self.params.update({'user_id' : self.id, 'text' : new_status})
-        response = requests.get(f'{self.API_BASE_URL}/status.set', params=self.params)
-        response.raise_for_status()
-
-
-    def replace_status(self, target, replace_str):
-        status = self.get_status()
-        new_status = status.replace(target, replace_str)
-        self.set_status(new_status)
-
-
     def get_photo(self):
         self.params.update({'owner_id' : self.id, 'album_id': 'wall', 'extended':1, 'count': 1000})
         response = requests.get(f'{self.API_BASE_URL}/photos.get', params=self.params)
